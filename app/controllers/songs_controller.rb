d48dcec497e8780c1e11872934d9a0e7a7fc6774
Moboluwarin <kolaogunnaike@gmail.com>
2017-07-09 19:36:37 +0200

    add images to cloudinary

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,7 +15,7 @@ class SongsController < ApplicationController
    end
 
   def create
-     @song = Song.new(song_params)
+     @song = Song.new(params[:Song])
 
     if @song.save
       redirect_to @song, notice: "Song Saved"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Artist.destroy_all
 Song.destroy_all
-artist1 = Artist.create!(email:"tolu@yahoo.com", name:"mary", image_url:"http://res.cloudinary.com/codaisseurify2/image/upload/v1499544561/sample.jpg", password:"1234abcd")
-artist2 = Artist.create!(email:"jimmy@yahoo.com", name:"Jimmy", image_url:"http://res.cloudinary.com/codaisseurify2/image/upload/v1499544561/sample.jpg", password:"1234abcd")
-artist3 = Artist.create!(email:"kim@yahoo.com", name:"Kim", image_url:"http://res.cloudinary.com/codaisseurify2/image/upload/v1499544561/sample.jpg", password:"1234abcd")
-artist4 = Artist.create!(email:"lima@yahoo.com", name:"Lima", image_url:"http://res.cloudinary.com/codaisseurify2/image/upload/v1499544561/sample.jpg", password:"1234abcd")
+artist1 = Artist.create!(email:"tolu@yahoo.com", name:"Fela", image_url:"http://res.cloudinary.com/codaisseurify2/image/upload/v1499621504/download_vhl1ni.jpg", password:"1234abcd")
+artist2 = Artist.create!(email:"jimmy@yahoo.com", name:"Davido", image_url:"http://res.cloudinary.com/codaisseurify2/image/upload/v1499621487/Davido_eprnia.jpg", password:"1234abcd")
+artist3 = Artist.create!(email:"kim@yahoo.com", name:"Michael", image_url:"http://res.cloudinary.com/codaisseurify2/image/upload/v1499621492/Michael-Jackson_vhiijm.jpg", password:"1234abcd")
+
 song1 = Song.create!(title: "Heal the world", song_url: "https://www.youtube.com", artist: artist1)
 song2 = Song.create!(title: "Heal the world", song_url: "https://www.youtube.com", artist: artist2)
 song3 = Song.create!(title: "Heal the world", song_url: "https://www.youtube.com", artist:artist3)
